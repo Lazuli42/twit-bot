799255cd73c8a685467909d925d725f2d4da34ed
anand angalig and zachary baum <anand@>
2016-10-13 09:58:05 -0400

    add streaming twit functionality with specific a location and usernames

--- js/bot.js
+++ js/bot.js
@@ -4,18 +4,26 @@ var Twit = require('twit');
 
 var twitBot = new Twit(apiKey);
 
-var parameters = {
-  q: '#wework',
-  count: 3,
-  lang: 'en',
-  geocode: "39.965876,-75.139439,5mi"
-}
+// var parameters = {
+//   q: 'from:Epicodus',
+//   count: 5,
+//   lang: 'en',
+//   //geocode: "39.965876,-75.139439,5000mi"
+// }
+//
+// twitBot.get('search/tweets', parameters, gotData);
+//
+// function gotData(err, data, response) {
+//   var tweets = data.statuses;
+//   for (var i = 0; i < tweets.length; i++) {
+//     console.log(tweets[i].text);
+//   }
+// }
 
-twitBot.get('search/tweets', parameters, gotData);
+var Philly = ['-75.148747', '39.958012', '-75.136237', '39.968899']
 
-function gotData(err, data, response) {
-  var tweets = data.statuses;
-  for (var i = 0; i < tweets.length; i++) {
-    console.log(tweets[i].text);
-  }
-}
+var stream = twitBot.stream('statuses/filter', { locations: Philly })
+
+stream.on('tweet', function (tweet) {
+  console.log(tweet.user.name + "@" + tweet.user.screen_name + ": " + tweet.text)
+})
